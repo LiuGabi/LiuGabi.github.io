9977c7b192a5e2b5724bd597ecf76ba410858fbd
Gabi <gabi@eyebuydirect.com>
2014-09-25 18:26:52 +0800

    ok

--- theme/js/animate/jsbook.js
+++ theme/js/animate/jsbook.js
@@ -1,1 +1,22 @@
-($)
+$(function() {
+
+	/*
+	 cover
+	 back-cover
+	 directory
+	 page-number
+	 */
+
+	$("#forward").click(function(e) {
+		
+		var pages = $(".page").not(".forward,.back");
+
+		var first = pages.first(),
+			second = pages.eq(1);
+		
+		first.addClass("forward");
+		second.addClass("back");
+
+	});
+
+});
