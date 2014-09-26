83e1562d12104b72ee6e7f52eb1f8e1cfc735443
Gabi <gabi@eyebuydirect.com>
2014-09-26 18:03:17 +0800

    gabi

--- theme/js/animate/jsbook.js
+++ theme/js/animate/jsbook.js
@@ -20,13 +20,14 @@ $(function() {
 		// current action page and next page
 		var current = pages.not(".odd-action,.even-action").first(),
 			next = current.next(),
-			prev = current.prev();
+			prev = current.prev(),
+			last = pages.last();
 
 		// layout z-index
 		var zIndex = 0;
 
 		if (prev.css("z-index")) {
-			zIndex = prev.css("z-index") - 1;
+			zIndex = prev.css("z-index") - last.css("z-index");
 		}
 
 		prev.css("z-index",zIndex);
