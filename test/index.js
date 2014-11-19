d792320ca5c4764466e8426794e6ca3e33c83744
Gabi <gabi@eyebuydirect.com>
2014-11-19 15:37:13 +0800

    ok

--- test/index.js
+++ test/index.js
@@ -1,101 +1,17 @@
-var fs = require("fs"),
-	path = require("path"),
-	http = require("http");
+// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
+var express = require('express');
+// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
+var app = express();
 
-var MINI = {
-	".css":"text/css",
-	".js":"application/javascript"
-};
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
 
-function combineFiles(pathnames, callback) {
-
-	var output = [];
-
-	(function next(i, len) {
-
-		if (i < len) {
-
-			fs.readFile(pathnames[i], function(err,data) {
-
-				if (err) {
-
-					callback(err);
-
-				} else {
-
-					output.push(data);
-					next(i+1,len);
-
-				}
-
-			});
-
-		} else {
-
-			callback(null, Buffer.concat(output));
-
-		}
-
-	}(0, pathnames.length));
-
-}
-
-function main(argv) {
-
-	var config = JSON.parse(fs.readFileSync(argv[0],"utf-8")),
-		root = config.root || ".",
-		port = config.port || 8080;
-
-	http.createServer(function(request, response) {
-
-		var urlInfo = parseUrl(root,request.url);
-
-		combineFiles(urlInfo.pathnames, function(err, data) {
-
-			if (err) {
-
-				response.writeHead(404);
-				response.end(err,message);
- 
-			} else {
-
-				response.writeHead(200, {"Content-Type": urlInfo.mime});
-
-			}
-
-			response.end(data);
-
-		});
-
-	}).listen(port);
-
-}
-
-function parseURL(root, url) {
-
-	var base, pathnames, parts;
-
-	if (url.indexOf("??") === -1) {
-
-		url = url.replace("/","/??");
-
-	}
-
-	parts = url.split("??");
-	base = parts[0];
-	pathnames = parts[1].split(",").map(function(value) {
-
-		return path.join(root,base,value);
-
-	});
-
-	return {
-
-		mime: MINI[path.extname(pathnames[0])] || "text/plain",
-		pathnames: pathnames
-
-	};
-
-}
-
-main(process.argv.slice(2));
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen(3000, function () {
+  console.log('app is listening at port 3000');
+});
