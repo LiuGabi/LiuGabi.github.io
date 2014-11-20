a18bbea3e29a689dcfe1e11d91c77cdfed77ec73
Gabi <gabi@eyebuydirect.com>
2014-11-20 12:09:42 +0800

    ok

--- approot/controller/HeaderController.js
+++ approot/controller/HeaderController.js
@@ -1,7 +1,7 @@
 angular.module("HeaderModule", [])
 .controller("HeaderController", function($scope) {
 
-	($scope.demo = "Demo";
+	$scope.demo = "Demo";
 	$scope.updateTitle = function() {
 		$scope.title = "That's a new title.";
 	}
