bbd7a318a22feab446b732cdb3d92a4000baf932
Gabi <gabi@eyebuydirect.com>
2014-11-20 12:29:31 +0800

    ok

--- approot/module/headerModule.js
+++ approot/module/headerModule.js
@@ -2,8 +2,5 @@ angular.module("headerModule", [])
 .controller("headerController", function($scope) {
 
 	$scope.demo = "Demo";
-	$scope.updateTitle = function() {
-		$scope.title = "That's a new title.";
-	}
 
 });
