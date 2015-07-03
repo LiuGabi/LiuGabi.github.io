b9b05579da18a1ffba1f1d48731ffd4c15a93baf
Gabi <gabi@eyebuydirect.com>
2015-07-03 18:19:58 +0800

    ok

--- d/xiaobo/routes/index.js
+++ d/xiaobo/routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* get home page. */
 router.get('/', function(req, res, next) {
 	res.render('default/', { title: 'XiaoBo' });
 });
 
-/* GET demo page. */
+/* get demo page. */
 router.get('/demo', function(req, res, next) {
 	res.render('demo/', { title: 'XiaoBo - Demo' });
+});
+
+/* get detail page. */
+router.get('/detail', function(req, res, next) {
+	res.render('default/detail', { title: 'XiaoBo - Detail' });
 });
 
 module.exports = router;
