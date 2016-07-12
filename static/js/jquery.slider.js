dc8588cb48d2104395a8d3b3afd0531389a2de38
Gabi <gabi@eyebuydirect.com>
2016-07-12 17:04:05 +0800

    js sidebar

---
+++ static/js/jquery.slider.js
@@ -0,0 +1,81 @@
+
+/**
+ * @example
+ *   $(document).ready(function(){
+ *      var slider = $(this).slider({
+ *          sidebar: $("#menu"),
+ *          container: $("#container"),
+ *          direction: 'left',
+ *          closefunction: function() {}
+ *      });
+ *      $("#button").click(function(){
+ *          slider.start();
+ *      })
+ *    });
+ */
+
+(function($) {
+    $.fn.slider = function(settings) {
+
+        var option = $.extend({}, $.fn.slider.defaults, settings || {});
+		var switchStatus = false;
+
+        var slide = {
+
+            adjust: function(){
+                $(window).on('orientationchange', function() {
+					slide.stop();
+                });
+            },
+
+            start: function(){
+
+				if (switchStatus === true) {
+					slide.stop();
+					return;
+				}
+
+				switchStatus= true;
+
+				if (option.statfunction && typeof option.statfunction == 'function') {
+                    option.statfunction.call();
+                }
+
+                if ('left' === option.direction) {
+					option.sidebar.addClass('slide-in')
+					option.container.addClass('shift-right');
+                }
+            },
+            
+            stop: function(){
+				switchStatus= false;
+				option.container.removeClass('shift-right');
+				option.sidebar.removeClass('slide-in');
+
+                if (option.closefunction && typeof option.closefunction == 'function') {
+                    option.closefunction.call();
+                }               
+            }
+        }
+
+        this.adjust = function(){
+            slide.adjust();
+        };
+        this.start = function(){
+            slide.start();
+        };
+        this.stop = function(){
+            slide.stop();
+        };
+
+        slide.adjust();
+
+        return this;
+    };
+
+    $.fn.slider.defaults = {
+        direction: 'left',
+        closefunction: false
+    };
+
+})(jQuery);
