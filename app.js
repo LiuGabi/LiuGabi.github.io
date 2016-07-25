02d28fbe2f65a4a0ab4d5b4e921eea6f81d32dac
Gabi <gabi@eyebuydirect.com>
2016-07-25 18:12:11 +0800

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+
+    // res.send("Hello Word");
+
+});
+
+app.listen(3000, function() {
+
+    console.log("App is listening at port 3000");    
+
+});
