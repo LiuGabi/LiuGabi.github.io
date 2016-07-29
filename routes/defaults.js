c63d21fcaecac2f1e930b46ad50edbee01aa1cff
Gabi <gabi@eyebuydirect.com>
2016-07-29 18:09:08 +0800

    update

--- routes/defaults.js
+++ routes/defaults.js
@@ -1,7 +1,23 @@
 var express = require("express");
-
 var defaults = express.Router();
 
+// var mongoose = require("mongoose");
+// 	mongoose.connect('mongodb://localhost/vblog');
+
+// var db = mongoose.connection;
+// db.on('error',console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+// 	console.log('We are connected.');
+// });
+
+var item = require('../models/item');
+
+
+
+// var query = item.find({}, {pic: true});
+
+
+/* get home page */
 defaults.get('/', function(req, res) {
 
 	res.render('layout/default', {
