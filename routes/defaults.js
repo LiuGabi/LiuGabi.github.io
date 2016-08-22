20afd8495ed5057954c5f422bde4e70bd09ae920
Gabi <a.min.liu.travel@gmail.com>
2016-08-22 23:27:48 +0800

    update

--- routes/defaults.js
+++ routes/defaults.js
@@ -1,29 +1,11 @@
 var express = require("express");
+
 var defaults = express.Router();
 
-// var itemModel = require('../models/itemModel');
 
 defaults.get('/', function(req, res) {
 
-	// var query =  itemModel.find({}, 'pic', function(err, results) {
-
-	// 	if (err) {
-
-	// 		console.log(err);
-
-	// 	} else {
-
-	// 		res.render('layout/default', {
-
-	// 			css: 'css/home.css',
-	// 			layout: '../home/index',
-	// 			item: results
-
-	// 		});
-
-	// 	}
-
-	// });	
+	res.render('../index');
 
 });
 
